470e333b3091be49593240a54b1b17e5d9108796
suryajai <arasidpharm@gmail.com>
2017-07-21 10:24:59 +0530

    Add files via upload

---
+++ interval.java
@@ -0,0 +1,36 @@
+
+public class interval {
+	public static void main(String[] args){
+	{		
+	       int i =0;
+	       int num =0;
+	       String  primeNumbers = "";
+
+	       for (i = 1; i <= 10; i++)         
+	       { 		  	  
+	          int counter=0; 	  
+	          for(num =i; num>=1; num--)
+		  {
+	             if(i%num==0)
+		     {
+	 		counter = counter + 1;
+		     }
+		  }
+		  if (counter ==2)
+		  {
+		     primeNumbers = primeNumbers + i + " ";
+		  }
+	       }	
+	       System.out.println("Prime numbers from 1 to 100 are :");
+	       System.out.println(primeNumbers);
+	   }
+	}
+}
+
+	
+	
+
+
+
+
+
